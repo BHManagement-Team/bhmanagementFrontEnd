a852e89798cd50857434a0bdfc86f2fbcbac20e7
My N <myEmail@example.com>
2019-11-16 12:29:51 +0800

    V

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+      <Sidebar v-if="LoginPage()"/>
+      <v-content>
+        <router-view/>
+      </v-content>
+  </v-app>
+</template>
+
+<script>
+import Sidebar from "@/components/Sidebar.vue";
+export default {
+  name: "App",
+  components:{
+    Sidebar
+  },
+  methods: {
+      LoginPage() {
+        if(this.$route.path == "/") {
+          return false
+        } else {
+          return true
+        }
+      }
+    }
+};
+</script>
---
+++ src/components/AddOccupantModal.vue
@@ -0,0 +1,19 @@
+
+<template>
+  <v-card-text>
+    <br>
+    <h3>ADD ROOM</h3>
+    <v-text-field label="Name" v-model="roomName" prepend-icon="mdi-home-edit"></v-text-field>
+    <v-text-field label="Room Number" v-model="roomType" prepend-icon="mdi-lock"></v-text-field>
+    <v-text-field label="Floor Number" v-model="roomSize" prepend-icon="mdi-lock"></v-text-field>
+    <v-text-field label="email" v-model="roomSharing" prepend-icon="mdi-account-group"></v-text-field>
+    <small class="grey--text">*This details will be save.</small>
+  </v-card-text>
+</template>
+    
+    <script>
+export default {
+  name: "Modal"
+};
+</script>
+    
