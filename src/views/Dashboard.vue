f8107f5fc11ff5e740fec74168bd12aa587f78c7
2ndyrGroupC <2ndyrGroupC@pn.local>
2019-11-26 21:19:14 +0800

    GGna

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -45,16 +45,16 @@
 
                   <v-card-actions>
                     <v-spacer></v-spacer>
-                    <v-btn class="ma-2" outlined color="error" @click="save">Cancel</v-btn>
-                    <v-btn class="ma-2" outlined color="success" @click="save">Save</v-btn>
+                    <v-btn class="ma-2" outlined color="error" @click="close()">Cancel</v-btn>
+                    <v-btn class="ma-2" outlined color="success" @click="save()">Save</v-btn>
                   </v-card-actions>
                 </v-card>
               </v-dialog>
             </v-toolbar>
           </template>
           <template v-slot:item.action="{ item }">
-            <v-icon small class="mr-2" @click="editItem(item)">mdi-pencil</v-icon>
-            <v-icon small @click="deleteItem('5ddcc5a40c88192be46eb397')">mdi-delete</v-icon>
+            <v-icon small class="mr-2" @click="editItem(room, room.room_id) ">mdi-pencil</v-icon>
+            <v-icon small @click="confirm = true">mdi-delete</v-icon>
           </template>
           <template v-slot:no-data>
             <v-btn color="primary">Reset</v-btn>
@@ -62,6 +62,22 @@
         </v-data-table>
       </v-col>
     </v-row>
+    <!-- confirmation Modal -->
+    <v-dialog v-model="confirm" max-width="500px" id="confirm">
+      <template v-slot:activator="{ on }">
+        <v-btn color="primary" dark class="mb-2" v-on="on">New Item</v-btn>
+      </template>
+      <v-card>
+        <v-card-title>
+          <span class="headline">Are you sure you want to delete?</span>
+        </v-card-title>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn class="ma-2" outlined color="error" @click="close()">Cancel</v-btn>
+          <v-btn class="ma-2" outlined color="success" @click="save()">Yes</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 <style>
@@ -88,6 +104,7 @@ function populateRoom() {
           roomCapacity: datax[counter].room_capacity,
           rentPrice: datax[counter].room_price
         });
+        // room.setAttribute("id",datax[counter]._id);
       }
     })
     .catch(error => {
@@ -97,6 +114,7 @@ function populateRoom() {
 }
 export default {
   data: () => ({
+    confirm: false,
     dialog: false,
     headers: [
       {
@@ -110,6 +128,7 @@ export default {
       { text: "Actions", value: "action", sortable: false }
     ],
     room: [],
+    room_id: "",
     editedIndex: -1,
     editedItem: {
       roomFloor: 1,
@@ -135,9 +154,9 @@ export default {
     }
   },
   methods: {
-    editItem(item) {
+    editItem(room, id) {
       axios
-        .post("http:/localhost:3000/bhm/updateRoom/"+ item, {
+        .post("http://localhost:3000/bhm/updateRoom/" + id, {
           room_name: this.editedItem.roomName,
           room_floor: this.editedItem.roomFloor,
           room_capacity: this.editedItem.roomCapacity,
@@ -153,12 +172,12 @@ export default {
       this.editedItem = Object.assign({}, item);
       this.dialog = true;
     },
-    deleteItem(item) {
-      const index = this.room.indexOf(item);
+    deleteItem(room, id) {
+      const index = this.room.indexOf(id);
       confirm("Are you sure you want to delete this item?");
       axios
-        .post("http://localhost:3000/bhm/deleteRoomByID/" + item, {
-          token: "dsfsdaf"
+        .delete("http://localhost:3000/bhm/deleteRoomByID/" + id, {
+          token: "sd"
         })
         .then(this.room.splice(index, 1))
         .catch(error => {
@@ -173,12 +192,11 @@ export default {
         this.editedIndex = -1;
       }, 300);
     },
-
-    save() {
+    save(item) {
       if (this.editedIndex > -1) {
         Object.assign(this.room[this.editedIndex], this.editedItem);
         axios
-          .post("http:/localhost:3000/bhm/updateRoom?method=boolean", {
+          .post("http:/localhost:3000/bhm/updateRoom/" + item, {
             room_name: this.editedItem.roomName,
             room_floor: this.editedItem.roomFloor,
             room_capacity: this.editedItem.roomCapacity,
