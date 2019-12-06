c35445eb7886b31bc220c41099efa986ddb38f15
Hannah Pelino <pelinoha_sd2053@ceaccs.net>
2019-12-06 14:44:21 +0800

    To be check

--- src/views/Analytics.vue
+++ src/views/Analytics.vue
@@ -1,16 +1,41 @@
 <template>
   <v-container fluid id="text">
     <v-row>
-      <v-card class="class female">
-        <h1>Number of Female Occupants:</h1>
-        <v-text-field v-model="female" prepend-icon="mdi-account-group" type="text" class="Text" disabled></v-text-field>
+      <v-card class="total">
+        <h2>Total Occupants:</h2>
+        <v-text-field
+          v-model="total"
+          prepend-icon="mdi-account-group"
+          type="text"
+          class="Text"
+          disabled
+        ></v-text-field>
+      </v-card>
+    </v-row>
+    <v-row>
+      <v-card class="female">
+        <img src="~@/assets/girl.png" id="girl" />
+
+        <h3>Female Occupants:</h3>
+        <v-text-field
+          v-model="female"
+          type="text"
+          class="Text"
+          disabled
+        ></v-text-field>
       </v-card>
     </v-row>
     <v-row>
       <div id="Male">
         <v-card class="class">
-        <h1>Number of Male Occupants:</h1>
-          <v-text-field v-model="male" prepend-icon="mdi-account-group-outline" type="text" class="Text" disabled></v-text-field>
+        <img src="~@/assets/man.png" id="man" />
+          <h3>Male Occupants:</h3>
+          <v-text-field
+            v-model="male"
+            type="text"
+            class="Text"
+            disabled
+          ></v-text-field>
         </v-card>
       </div>
     </v-row>
@@ -20,24 +45,50 @@
 .class {
   width: 500px !important;
   padding: 20px;
+  padding: 20px;
+  padding-left: 150px !important;
 }
 .female {
   position: absolute;
   top: 300px;
-  left: 250px;
+  left: 350px;
+  padding: 20px;
+  padding-left: 150px !important;
+  max-width: 350px !important;
+
 }
 #Male {
   position: absolute;
   top: 300px;
-  left: 850px;
+  left: 790px;
+  max-width: 350px !important;
+
 }
-.Text{
-    font-size: 20px!important;
-    color: black!important;
+.Text {
+  font-size: 20px !important;
+  color: black !important;
 }
-#text{
+#text {
   background-color: rgb(224, 224, 224);
-  height: 100%  ;
+  height: 100%;
+}
+.total {
+  position: absolute;
+  top: 150px;
+  left: 600px;
+  max-height: 130px;
+  max-width: 550px !important;
+  padding: 10px;
+}
+#girl{
+  position: absolute;
+  top: 10px;
+  left:20px;
+}
+#man{
+  position: absolute;
+  top: 15px;
+  left:30px;
 }
 </style>
 <script>
@@ -46,7 +97,8 @@ export default {
   data() {
     return {
       female: "",
-      male:"",
+      male: "",
+      total: "",
       headers: [
         {
           text: "Number of Female Occupants",
@@ -75,6 +127,17 @@ export default {
         .then(response => {
           console.log(response);
           this.male = response.data.data;
+        })
+        .catch(err => {
+          console.log(err);
+        });
+
+      Axios.post("http://localhost:3000/bhm/total", {
+        token: localStorage.token
+      })
+        .then(response => {
+          console.log(response);
+          this.total = response.data.data;
         })
         .catch(err => {
           console.log(err);
