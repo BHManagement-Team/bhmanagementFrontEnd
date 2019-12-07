4c21eea8db56824a2ee22e88c59aee8ec27b53a1
nanitkatipunan <nanit.katipunan@student.passerellesnumeriques.org>
2019-12-07 16:52:31 +0800

    edit

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,170 +1,170 @@
 <template>
   <div id="body">
     <div>
-    <v-row>
-      <v-col>
-        <v-data-table
-          :headers="headers"
-          :items="room"
-          sort-by="roomName"
-          class="elevation-1"
-          id="table"
-          :search="search"
-        >
-          <template v-slot:top>
-            <v-toolbar flat color="white">
-              <v-toolbar-title>Room Management</v-toolbar-title>
-              <v-divider class="mx-4" inset vertical></v-divider>
-              <v-text-field
-                v-model="search"
-                append-icon="mdi-magnify"
-                label="Search"
-                single-line
-                hide-details
-              ></v-text-field>
-              <v-spacer></v-spacer>
+      <v-row>
+        <v-col>
+          <v-data-table
+            :headers="headers"
+            :items="room"
+            sort-by="roomName"
+            class="elevation-1"
+            id="table"
+            :search="search"
+          >
+            <template v-slot:top>
+              <v-toolbar flat color="white">
+                <v-toolbar-title>Room Management</v-toolbar-title>
+                <v-divider class="mx-4" inset vertical></v-divider>
+                <v-text-field
+                  v-model="search"
+                  append-icon="mdi-magnify"
+                  label="Search"
+                  single-line
+                  hide-details
+                ></v-text-field>
+                <v-spacer></v-spacer>
 
-              <v-dialog v-model="dialog" max-width="500px">
-                <template v-slot:activator="{ on }">
-                  <v-btn color="primary" dark class="mb-2" v-on="on">Add Room</v-btn>
-                </template>
-                <v-form ref="form" v-model="valid" lazy-validation>
-                  <v-card>
-                    <v-card-title>
-                      <span class="headline">{{ formTitle }}</span>
-                    </v-card-title>
-                    <v-card-text>
-                      <v-container>
-                        <v-row>
-                          <v-col cols="12" sm="6" md="4">
-                            <v-text-field
-                              v-model="editedItem.roomFloor"
-                              label="Room Floor"
-                              :rules="floorRules"
-                            ></v-text-field>
-                          </v-col>
-                          <v-col cols="12" sm="6" md="4">
-                            <v-text-field
-                              v-model="editedItem.roomName"
-                              label="Room Name"
-                              :rules="nameRules"
-                            ></v-text-field>
-                          </v-col>
-                          <v-col cols="12" sm="6" md="4">
-                            <v-text-field
-                              v-model="editedItem.roomCapacity"
-                              label="Room Capacity"
-                              :rules="capacityRules"
-                              type="number"
-                            ></v-text-field>
-                          </v-col>
-                          <v-col cols="12" sm="6" md="4">
-                            <v-text-field
-                              v-model="editedItem.rentPrice"
-                              label="Rent Price"
-                              :rules="priceRules"
-                              type="number"
-                            ></v-text-field>
-                          </v-col>
-                        </v-row>
-                      </v-container>
-                    </v-card-text>
-                    <v-card-actions>
-                      <v-spacer></v-spacer>
-                      <v-btn class="ma-2" outlined color="error" @click="close()">Cancel</v-btn>
-                      <v-btn
-                        class="ma-2"
-                        outlined
-                        color="success"
-                        @click="save"
-                        :disabled="!valid"
-                      >Save</v-btn>
-                    </v-card-actions>
-                  </v-card>
-                </v-form>
-              </v-dialog>
-            </v-toolbar>
-          </template>
-          <template v-slot:item.action="{ item }">
-            <v-icon small class="mr-2" @click="editItem(item)">mdi-pencil</v-icon>
-            <v-icon small @click="openDialog(item.number)">mdi-delete</v-icon>
-          </template>
-        </v-data-table>
-      </v-col>
-    </v-row>
-    <!-- confirmation Modal -->
-    <v-dialog v-model="confirm" max-width="500px" id="confirm">
-      <v-card>
-        <center>
-          <img src="~@/assets/del.gif" id="delImg">
-        </center>
-        <v-card-title>
-          <span class="headline">Are you sure you want to delete?</span>
-        </v-card-title>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <br>
-          <v-btn class="ma-2" outlined color="error" @click="close()">Cancel</v-btn>
-          <v-btn class="ma-2" outlined color="success" @click="deleteItem(currentId)">Yes</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-    <!-- end -->
-    <v-dialog v-model="success" max-width="500px" id="confirm">
-      <v-card>
-        <v-card-title>
+                <v-dialog v-model="dialog" max-width="500px">
+                  <template v-slot:activator="{ on }">
+                    <v-btn color="primary" dark class="mb-2" v-on="on">Add Room</v-btn>
+                  </template>
+                  <v-form ref="form" v-model="valid" lazy-validation>
+                    <v-card>
+                      <v-card-title>
+                        <span class="headline">{{ formTitle }}</span>
+                      </v-card-title>
+                      <v-card-text>
+                        <v-container>
+                          <v-row>
+                            <v-col cols="12" sm="6" md="4">
+                              <v-text-field
+                                v-model="editedItem.roomFloor"
+                                label="Room Floor"
+                                :rules="floorRules"
+                              ></v-text-field>
+                            </v-col>
+                            <v-col cols="12" sm="6" md="4">
+                              <v-text-field
+                                v-model="editedItem.roomName"
+                                label="Room Name"
+                                :rules="nameRules"
+                              ></v-text-field>
+                            </v-col>
+                            <v-col cols="12" sm="6" md="4">
+                              <v-text-field
+                                v-model="editedItem.roomCapacity"
+                                label="Room Capacity"
+                                :rules="capacityRules"
+                                type="number"
+                              ></v-text-field>
+                            </v-col>
+                            <v-col cols="12" sm="6" md="4">
+                              <v-text-field
+                                v-model="editedItem.rentPrice"
+                                label="Rent Price"
+                                :rules="priceRules"
+                                type="number"
+                              ></v-text-field>
+                            </v-col>
+                          </v-row>
+                        </v-container>
+                      </v-card-text>
+                      <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn class="ma-2" outlined color="error" @click="close()">Cancel</v-btn>
+                        <v-btn
+                          class="ma-2"
+                          outlined
+                          color="success"
+                          @click="save"
+                          :disabled="!valid"
+                        >Save</v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </v-form>
+                </v-dialog>
+              </v-toolbar>
+            </template>
+            <template v-slot:item.action="{ item }">
+              <v-icon small class="mr-2" @click="editItem(item)">mdi-pencil</v-icon>
+              <v-icon small @click="openDialog(item.number)">mdi-delete</v-icon>
+            </template>
+          </v-data-table>
+        </v-col>
+      </v-row>
+      <!-- confirmation Modal -->
+      <v-dialog v-model="confirm" max-width="500px" id="confirm">
+        <v-card>
           <center>
-            <img src="~@/assets/suc.gif" id="successImg">
+            <img src="~@/assets/del.gif" id="delImg">
           </center>
-          <br>
-          <h2 class="headline">Room has been added successfully!!</h2>
-          <br>
-          <v-spacer></v-spacer>
-          <br>
-          <br>
-          <v-btn class="btnClose" outlined color="success" @click="closesuccess()">C L O S E</v-btn>
-          <br>
-          <v-spacer></v-spacer>
-        </v-card-title>
-      </v-card>
-    </v-dialog>
-    <v-dialog v-model="deleteconfirm" max-width="500px" id="confirm">
-      <v-card>
-        <v-card-title>
-          <center>
-            <img src="~@/assets/suc.gif" id="successImg">
-          </center>
-          <br>
-          <h2 class="headline">Room has been deleted successfully!!</h2>
-          <br>
-          <v-spacer></v-spacer>
-          <br>
-          <br>
-          <v-btn class="btnClose" outlined color="success" @click="closedelete()">C L O S E</v-btn>
-          <br>
-          <v-spacer></v-spacer>
-        </v-card-title>
-      </v-card>
-    </v-dialog>
-    <v-dialog v-model="updated" max-width="500px" id="confirm">
-      <v-card>
-        <v-card-title>
-          <center>
-            <img src="~@/assets/suc.gif" id="successImg">
-          </center>
-          <br>
-          <h2 class="headline">Room has been updated successfully!!</h2>
-          <br>
-          <v-spacer></v-spacer>
-          <br>
-          <br>
-          <v-btn class="btnClose" outlined color="success" @click="cloeseupdate()">C L O S E</v-btn>
-          <br>
-          <v-spacer></v-spacer>
-        </v-card-title>
-      </v-card>
-    </v-dialog>
-  </div>
+          <v-card-title>
+            <span class="headline">Are you sure you want to delete?</span>
+          </v-card-title>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <br>
+            <v-btn class="ma-2" outlined color="error" @click="close()">Cancel</v-btn>
+            <v-btn class="ma-2" outlined color="success" @click="deleteItem(currentId)">Yes</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+      <!-- end -->
+      <v-dialog v-model="success" max-width="500px" id="confirm">
+        <v-card>
+          <v-card-title>
+            <center>
+              <img src="~@/assets/suc.gif" id="successImg">
+            </center>
+            <br>
+            <h2 class="headline">Room has been added successfully!!</h2>
+            <br>
+            <v-spacer></v-spacer>
+            <br>
+            <br>
+            <v-btn class="btnClose" outlined color="success" @click="closesuccess()">C L O S E</v-btn>
+            <br>
+            <v-spacer></v-spacer>
+          </v-card-title>
+        </v-card>
+      </v-dialog>
+      <v-dialog v-model="deleteconfirm" max-width="500px" id="confirm">
+        <v-card>
+          <v-card-title>
+            <center>
+              <img src="~@/assets/suc.gif" id="successImg">
+            </center>
+            <br>
+            <h2 class="headline">Room has been deleted successfully!!</h2>
+            <br>
+            <v-spacer></v-spacer>
+            <br>
+            <br>
+            <v-btn class="btnClose" outlined color="success" @click="closedelete()">C L O S E</v-btn>
+            <br>
+            <v-spacer></v-spacer>
+          </v-card-title>
+        </v-card>
+      </v-dialog>
+      <v-dialog v-model="updated" max-width="500px" id="confirm">
+        <v-card>
+          <v-card-title>
+            <center>
+              <img src="~@/assets/suc.gif" id="successImg">
+            </center>
+            <br>
+            <h2 class="headline">Room has been updated successfully!!</h2>
+            <br>
+            <v-spacer></v-spacer>
+            <br>
+            <br>
+            <v-btn class="btnClose" outlined color="success" @click="cloeseupdate()">C L O S E</v-btn>
+            <br>
+            <v-spacer></v-spacer>
+          </v-card-title>
+        </v-card>
+      </v-dialog>
+    </div>
   </div>
 </template>
 <style>
@@ -193,38 +193,13 @@
 .headline {
   margin-left: 35px !important;
 }
-#body{
+#body {
   background-color: rgb(224, 224, 224);
-  height: 100%  ;
+  height: 100%;
 }
 </style>
 <script>
 import axios from "axios";
-function populateRoom() {
-  var room = [];
-  axios
-    .post("http://172.16.3.20:3000/bhm/retrieveAllRooms", {
-      token: localStorage.token
-    })
-    .then(response => {
-      console.log(response);
-      var datax = response.data.data;
-      var counter = 0;
-      for (counter; counter < datax.length; counter++) {
-        room.push({
-          number: datax[counter]._id,
-          roomFloor: datax[counter].room_floor,
-          roomName: datax[counter].room_name,
-          roomCapacity: datax[counter].room_capacity,
-          rentPrice: datax[counter].room_price
-        });
-      }
-    })
-    .catch(error => {
-      console.log(error);
-    });
-  return room;
-}
 export default {
   data: () => ({
     search: "",
@@ -259,6 +234,14 @@ export default {
       roomName: "",
       roomCapacity: "",
       rentPrice: ""
+    },
+    //temporary value for editing
+    ItemInRow:{
+      number: "",
+      roomFloor: "",
+      roomName: "",
+      roomCapacity: "",
+      rentPrice: ""
     }
   }),
   computed: {
@@ -269,7 +252,7 @@ export default {
     }
   },
   mounted() {
-    this.room = populateRoom();
+    this.populateRoom();
   },
 
   watch: {
@@ -278,9 +261,33 @@ export default {
     }
   },
   methods: {
+    populateRoom() {
+  
+      axios
+        .post("http://localhost:3000/bhm/retrieveAllRooms", {
+          token: this.$store.state.token
+        })
+        .then(response => {       
+          var datax = response.data.data;
+          var counter = 0;
+          for (counter; counter < datax.length; counter++) {
+            this.room.push({
+              number: datax[counter]._id,
+              roomFloor: datax[counter].room_floor,
+              roomName: datax[counter].room_name,
+              roomCapacity: datax[counter].room_capacity,
+              rentPrice: datax[counter].room_price
+            });
+          }
+        })
+        .catch(error => {
+          console.log("fucking "+error);
+        });
+    },
     editItem(item) {
       this.editedIndex = this.room.indexOf(item);
       this.editedItem = Object.assign({}, item);
+      console.log(this.editedItem)
       this.dialog = true;
     },
     openDialog(id) {
@@ -298,7 +305,7 @@ export default {
     deleteItem(id) {
       const index = this.room.indexOf(id);
       axios
-        .post("http://172.16.3.20:3000/bhm/deleteRoomByID/" + id, {
+        .post("http://localhost:3000/bhm/deleteRoomByID/" + id, {
           token: localStorage.token
         })
         .then(response => {
@@ -337,10 +344,10 @@ export default {
         }
       } else {
         if (this.editedIndex > -1) {
-          Object.assign(this.room[this.editedIndex], this.editedItem);
           axios
             .post(
-              "http://172.16.3.20:3000/bhm/updateRoom/" + this.editedItem.number,
+              "http://localhost:3000/bhm/updateRoom/" +
+                this.editedItem.number,
               {
                 _id: this.editedItem.number,
                 room_name: this.editedItem.roomName,
@@ -350,34 +357,37 @@ export default {
                 token: localStorage.token
               }
             )
-            .then(response => {
-              console.log(response);
-              this.occupant.push = this.populateRoom();
+            .then(() => {
+              console.log(this.editedItem.roomName)
+              Object.assign(
+              this.room[this.editedIndex],
+              this.editedItem
+            );
               this.showupdated();
+              this.close();
             })
             .catch(error => {
+
               console.log(error);
             });
         } else if (this.$refs.form.validate()) {
-          this.snackbar = true;
-          this.room.push(this.editedItem);
+          alert("I'm saving it now");
+          this.snackbar = true;     
           axios
-            .post("http://172.16.3.20:3000/bhm/createRoom", {
+            .post("http://localhost:3000/bhm/createRoom", {
               room_name: this.editedItem.roomName,
               room_floor: this.editedItem.roomFloor,
               room_capacity: this.editedItem.roomCapacity,
               room_price: this.editedItem.rentPrice,
-              token: localStorage.token
+              token: this.$store.state.token
             })
-            .then((this.room.push = populateRoom()), this.showSuccess())
-            .catch(error => {
+            .then(this.room.push(this.editedItem),this.showSuccess(),this.close())
+            .catch(error => {            
               console.log(error);
             });
         }
       }
-
-      this.$refs.form.resetValidation();
-      this.close();
+      
     }
   }
 };
